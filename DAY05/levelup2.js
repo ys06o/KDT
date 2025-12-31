342b2c8cca9608d1884a328d11be6e84859471a0
ys06 <yongseongg00@gmail.com>
2025-12-31 18:17:32 +0900

    12/31일 결과물

--- DAY05/levelup2.js
+++ DAY05/levelup2.js
@@ -1,30 +1,26 @@
-let carNumberList=['12가3456','34나7890','56다1234'];
-let useTimeList=[45,130,320];
+let carNumberList = ['12가3456', '34나7890', '56다1234'];
+let useTimeList = [45, 130, 320];
 
-let num=prompt("차량번호를입력해주세요:");
-
-let overtime;
-let charge=1000;
-if(carNumberList.indexOf(num)==-1){
-  console.log("존재하지않는차량입니다.")
+let num = prompt("차량번호를입력해주세요:");
+let re=0;
+let overtime = useTimeList.indexOf(num);
+let result=parseInt((useTimeList[overtime] - 60) / 30);
+if ((useTimeList[overtime]-60)%30>0){
+  result+=1
 }else{
-if(num<=60){
-    console.log("요금:1000원");
+  result+=0
 }
-else if(num>60){
-  overtime=num-60;
-}
-}
-
-if(overtime>=0&&overtime<=30){
-  charge+=500;
-}
-else if(overtime>30&&overtime<=60){
-  charge+=1000;
-}
-else if(overtime>60&&overtime<=90){
-  charge+=1000;
-}
-else if(charge>10000){
-  
+if (overtime == -1) {
+  console.log("존재하지않는차량입니다.")
+} else{
+  if(useTimeList[overtime]<=60){
+    re=1000;
+    console.log(re);
+  }else if(useTimeList[overtime]<=600){
+    re=1000+500*result;
+    console.log(re);
+  }else{
+    re=10000;
+    console.log(re);
+  }
 }
